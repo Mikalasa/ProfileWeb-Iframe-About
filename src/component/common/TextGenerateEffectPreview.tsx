9e6a58a40d5d4173d7848c91cc2f88a180d48787
gezhe <htt3001ptq@hotmail.com>
2024-02-21 08:46:08 +1300

    add useInView for text generate

--- src/component/common/TextGenerateEffectPreview.tsx
+++ src/component/common/TextGenerateEffectPreview.tsx
@@ -1,5 +1,5 @@
-"use client";
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
+import { useInView } from 'react-intersection-observer';
 import { TextGenerateEffect } from "./text-generate-effect";
 
 const words = `Outside the realm of programming, I also have a deep fondness for 3D art, 
@@ -7,29 +7,14 @@ passionate about creating and rendering scenes from the imagination.
 Using technology to create beauty is a pursuit worth undertaking.`;
 
 export function TextGenerateEffectPreview() {
-    const [isVisible, setIsVisible] = useState(false);
-    const ref = useRef(null);
-
-    useEffect(() => {
-        const observer = new IntersectionObserver((entries) => {
-            if (entries[0].isIntersecting) {
-                setIsVisible(true);
-                observer.disconnect();
-            }
-        });
-
-        if (ref.current) {
-            observer.observe(ref.current);
-        }
-
-        return () => {
-            observer.disconnect();
-        };
-    }, []);
+    const { ref, inView } = useInView({
+        triggerOnce: true,
+        threshold: 0.5,
+    });
 
     return (
         <div ref={ref}>
-            {isVisible && <TextGenerateEffect words={words} />}
+            {inView && <TextGenerateEffect words={words} />}
         </div>
     );
 }
